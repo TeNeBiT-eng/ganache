6db871fe57f1d4a78c2626c333a8a7b72a55a5d6
Jeff Smale <6363749+jeffsmale90@users.noreply.github.com>
2022-06-01 12:21:00 +1200

    Consume new Quantity.add() and Quantity.multiply() functions, tidy up some Quantity.from Quantity.toBigInt permutations.

--- src/chains/ethereum/ethereum/src/blockchain.ts
+++ src/chains/ethereum/ethereum/src/blockchain.ts
@@ -554,9 +554,8 @@ export default class Blockchain extends Emittery<BlockchainTypedEvents> {
 
   #readyNextBlock = (previousBlock: Block, timestamp?: number) => {
     const previousHeader = previousBlock.header;
-    const previousNumber = previousHeader.number.toBigInt() || 0n;
     return new RuntimeBlock(
-      Quantity.from(previousNumber + 1n),
+      previousHeader.number.add(1n),
       previousBlock.hash(),
       this.coinbase,
       this.#options.miner.blockGasLimit.toBuffer(),
@@ -696,7 +695,7 @@ export default class Blockchain extends Emittery<BlockchainTypedEvents> {
         }
       }
       const genesis = new RuntimeBlock(
-        Quantity.from(fallbackBlock.header.number.toBigInt() + 1n),
+        fallbackBlock.header.number.add(1n),
         fallbackBlock.hash(),
         this.coinbase,
         blockGasLimit.toBuffer(),
@@ -1072,9 +1071,7 @@ export default class Blockchain extends Emittery<BlockchainTypedEvents> {
         buf: caller
       } as any);
       fromAccount.nonce.iaddn(1);
-      const txCost = new BN(
-        (gasLimit * transaction.gasPrice.toBigInt()).toString()
-      );
+      const txCost = new BN(transaction.gasPrice.multiply(gasLimit).toBigInt().toString());
       fromAccount.balance.isub(txCost);
       await vm.stateManager.putAccount({ buf: caller } as any, fromAccount);
 
@@ -1354,7 +1351,7 @@ export default class Blockchain extends Emittery<BlockchainTypedEvents> {
   } => {
     // Prepare the "next" block with necessary transactions
     const newBlock = new RuntimeBlock(
-      Quantity.from((parentBlock.header.number.toBigInt() || 0n) + 1n),
+      parentBlock.header.number.add(1n),
       parentBlock.hash(),
       parentBlock.header.miner,
       parentBlock.header.gasLimit.toBuffer(),
